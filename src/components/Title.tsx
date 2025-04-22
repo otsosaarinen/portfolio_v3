aa92b9517b08f5e6541b229f84fd928d480a6439
Otso Saarinen <saarinenotso@gmail.com>
2025-04-22 18:15:47 +0300

    create base title.tsx

---
+++ src/components/Title.tsx
@@ -0,0 +1,11 @@
+function Title() {
+    return (
+        <>
+            <div className="font-nunito-sans text-8xl font-black italic">
+                Otso Saarinen
+            </div>
+        </>
+    );
+}
+
+export default Title;
