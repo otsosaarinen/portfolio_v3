0e7acb8af3438b2d25062de96072fb94809cac25
Otso Saarinen <saarinenotso@gmail.com>
2025-04-22 20:16:45 +0300

    create logic for looping through the skill list

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,41 @@
 import Typewriter from "react-ts-typewriter";
 
 function Title() {
+    type SkillType = {
+        [skill: string]: string[];
+    };
+
+    const skills: SkillType[] = [
+        {
+            Frontend: [
+                "TypeScript",
+                "JavaScript",
+                "React",
+                "Tailwind CSS",
+                "HTML",
+            ],
+        },
+        { Backend: ["Node.js", "Express.js", "Python", "Flask", "Postman"] },
+        {
+            "Cloud & database": [
+                "Apache Kafka",
+                "Microsoft Azure",
+                "PostgreSQL",
+                "SQLite",
+                "InfluxDB",
+            ],
+        },
+        {
+            Miscellaneous: [
+                "REST API",
+                "OAuth 2.0",
+                "Gen AI",
+                "Grafana",
+                "GitHub",
+            ],
+        },
+    ];
+
     return (
         <>
             <div className="font-nunito-sans flex flex-col items-center justify-center gap-5 text-center text-white">
@@ -18,6 +53,23 @@ function Title() {
                         speed={20}
                         cursor={false}
                     />
+                </div>
+                <div className="">
+                    <div className="text-4xl font-bold text-blue-500 italic">
+                        Skills
+                    </div>
+                    <div className="grid grid-cols-4 gap-5">
+                        {skills.map((skill, index) => {
+                            const categoryName = Object.keys(skill)[0];
+                            const singleSkill = skill;
+                            console.log(singleSkill[categoryName]);
+                            return (
+                                <div key={index} className="text-xl">
+                                    {categoryName}
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </>
