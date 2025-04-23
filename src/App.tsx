96cfa2ed2bff597d917e3c905649ad91dddc3c73
Otso Saarinen <saarinenotso@gmail.com>
2025-04-24 00:29:17 +0300

    import all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Info from "./components/Info";
 
 function App() {
     return (
         <>
             <div className="font-nunito-sans h-screen w-screen bg-[url('/coding.jpg')] bg-cover bg-no-repeat">
-                <div className="flex h-full w-full flex-col gap-5 bg-neutral-900/60 text-neutral-800">
+                <div className="flex h-full w-full flex-col bg-neutral-900/70 text-neutral-800">
                     <Header />
-                    <Skills />
+                    <Info />
                 </div>
             </div>
         </>
