13792018515c9cac0ef68db716398936b081ae2a
Otso Saarinen <saarinenotso@gmail.com>
2025-04-24 00:29:23 +0300

    change padding values

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <>
-            <div className="flex flex-col bg-white px-5 py-5 text-indigo-500">
+            <div className="flex flex-col bg-white p-5 text-indigo-500">
                 <h1 className="text-8xl font-black">Otso Saarinen</h1>
                 <h2 className="text-3xl font-bold italic">
                     Bachelor of Engineering, Information and Communications
