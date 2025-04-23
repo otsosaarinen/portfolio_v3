d5bef2f429236cc321a049fd4bcbf00985e4d922
Otso Saarinen <saarinenotso@gmail.com>
2025-04-23 16:30:42 +0300

    add background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import Header from "./components/Header";
-import Content from "./components/Title";
+import Content from "./components/Content";
 import Footer from "./components/Footer";
 
 function App() {
     return (
         <>
-            <div className="font-nunito-sans flex min-h-screen w-screen flex-col items-center justify-start">
-                <div className="w-full bg-blue-400">
+            <div className="font-nunito-sans flex min-h-screen w-screen flex-col items-center justify-start text-neutral-900">
+                <div className="w-full bg-blue-400 p-5">
                     <Header />
                 </div>
-                <div className="w-full flex-grow bg-white">
+                <div className="w-full flex-grow bg-white bg-[url('/coding.jpg')] bg-cover bg-no-repeat">
                     <Content />
                 </div>
-                <div className="w-full bg-blue-400">
+                <div className="w-full bg-blue-400 p-5">
                     <Footer />
                 </div>
             </div>
