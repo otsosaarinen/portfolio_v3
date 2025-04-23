92dffb762cca80be8907077fd0a248def430907a
Otso Saarinen <saarinenotso@gmail.com>
2025-04-23 13:26:25 +0300

    add links to github & linkedin

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -38,7 +38,7 @@ function Title() {
 
     return (
         <>
-            <div className="font-nunito-sans bg-dark-purple flex h-full min-h-screen w-full flex-col items-center justify-center gap-10 text-center text-white">
+            <div className="font-nunito-sans bg-dark-purple flex h-full min-h-screen w-full flex-col items-center justify-center gap-10 py-5 text-center text-white">
                 <div>
                     <h1 className="text-honeydew text-8xl font-black">
                         Otso Saarinen
@@ -47,6 +47,7 @@ function Title() {
                         Bachelor of Engineering
                     </h2>
                 </div>
+
                 <div className="text-honeydew max-w-md text-xl md:max-w-2xl">
                     <Typewriter
                         text="I'm a recently graduated ICT engineer from Turku UAS,
@@ -56,11 +57,40 @@ function Title() {
                         cursor={false}
                     />
                 </div>
-                <div className="flex flex-col">
+
+                <div className="flex flex-col items-center justify-center gap-2">
+                    <div className="text-naples-yellow text-2xl font-bold italic">
+                        Connect with me
+                    </div>
+                    <div className="flex flex-row gap-3">
+                        <a
+                            href="https://github.com/otsosaarinen"
+                            target="_blank"
+                        >
+                            <img
+                                className="h-auto w-10"
+                                src="/github_logo.png"
+                                alt="GitHub logo"
+                            />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/otsosaarinen/"
+                            target="_blank"
+                        >
+                            <img
+                                className="h-auto w-10"
+                                src="/linkedin_logo.png"
+                                alt="LinkedIn logo"
+                            />
+                        </a>
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-2">
                     <div className="text-honeydew text-4xl font-bold">
                         Skills
                     </div>
-                    <div className="grid grid-cols-2 md:grid-cols-4">
+                    <div className="grid grid-cols-1 gap-5 md:grid-cols-4 md:gap-0">
                         {skills.map((skill, index) => {
                             const categoryName = Object.keys(skill)[0];
                             const singleSkill = skill;
