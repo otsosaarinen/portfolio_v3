f6c33de751843734ebe6b7633facadeafab93d01
Otso Saarinen <saarinenotso@gmail.com>
2025-04-24 00:02:01 +0300

    rename Content to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,12 @@
 function Header() {
     return (
         <>
-            <div className="flex flex-row items-center justify-center gap-3 p-2 text-3xl font-bold text-white">
-                <div>Welcome to my portfolio</div>
-                <div className="flex flex-row gap-3">
-                    <a href="https://github.com/otsosaarinen" target="_blank">
-                        <img
-                            src="github_logo.png"
-                            alt="Logo of GitHub"
-                            className="h-8 w-8"
-                        />
-                    </a>
-                    <a
-                        href="https://www.linkedin.com/in/otsosaarinen/"
-                        target="_blank"
-                    >
-                        <img
-                            src="linkedin_logo.png"
-                            alt="Logo of LinkedIn"
-                            className="h-8 w-8"
-                        />
-                    </a>
-                </div>
+            <div className="flex flex-col bg-white px-5 py-5 text-indigo-500">
+                <h1 className="text-8xl font-black">Otso Saarinen</h1>
+                <h2 className="text-3xl font-bold italic">
+                    Bachelor of Engineering, Information and Communications
+                    Technology
+                </h2>
             </div>
         </>
     );
