fd8bda854372549ecfb272965acd16cf5b6fb959
Otso Saarinen <saarinenotso@gmail.com>
2025-04-24 00:03:09 +0300

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -36,29 +36,35 @@ function Skills() {
 
     return (
         <>
-            <div className="grid grid-cols-4 text-white">
-                {skills.map((skill, index) => {
-                    const categoryName = Object.keys(skill)[0];
-                    const singleSkill = skill;
-                    return (
-                        <div key={index} className="flex flex-col">
-                            <div className="text-2xl font-bold">
-                                {categoryName}
+            <div className="flex flex-col gap-3 px-5">
+                <div className="text-3xl font-bold text-white">My Skills</div>
+                <div className="grid h-50 w-250 grid-cols-4 gap-3">
+                    {skills.map((skill, index) => {
+                        const categoryName = Object.keys(skill)[0];
+                        const singleSkill = skill;
+                        return (
+                            <div
+                                key={index}
+                                className="flex flex-col justify-around bg-white p-2 shadow-xl shadow-white/10"
+                            >
+                                <div className="text-2xl font-bold">
+                                    {categoryName}
+                                </div>
+                                <div className="text-xl font-medium">
+                                    {singleSkill[categoryName].map(
+                                        (skillName, skillIndex) => {
+                                            return (
+                                                <div key={skillIndex}>
+                                                    {skillName}
+                                                </div>
+                                            );
+                                        },
+                                    )}
+                                </div>
                             </div>
-                            <div className="text-xl">
-                                {singleSkill[categoryName].map(
-                                    (skillName, skillIndex) => {
-                                        return (
-                                            <div key={skillIndex}>
-                                                {skillName}
-                                            </div>
-                                        );
-                                    },
-                                )}
-                            </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                </div>
             </div>
         </>
     );
