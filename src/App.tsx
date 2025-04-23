1178c81196f4455337840fb1ed15958e2b8f2187
Otso Saarinen <saarinenotso@gmail.com>
2025-04-23 15:44:12 +0300

    rewrite the app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import "./App.css";
-import Title from "./components/Title";
+import Header from "./components/Header";
+import Content from "./components/Title";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <>
-            <div className="flex flex-col items-center justify-center">
-                <Title />
+            <div className="font-nunito-sans flex min-h-screen w-screen flex-col items-center justify-start">
+                <div className="w-full bg-blue-400">
+                    <Header />
+                </div>
+                <div className="w-full flex-grow bg-white">
+                    <Content />
+                </div>
+                <div className="w-full bg-blue-400">
+                    <Footer />
+                </div>
             </div>
         </>
     );
