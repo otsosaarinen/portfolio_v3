33c8c33d1d67795d8fc5b7d8685b20f969634e0f
Otso Saarinen <saarinenotso@gmail.com>
2025-04-23 12:55:27 +0300

    add css variables

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -38,14 +38,16 @@ function Title() {
 
     return (
         <>
-            <div className="font-nunito-sans flex h-full w-full flex-col items-center justify-center gap-10 text-center text-white">
+            <div className="font-nunito-sans bg-dark-purple flex h-full min-h-screen w-full flex-col items-center justify-center gap-10 text-center text-white">
                 <div>
-                    <h1 className="text-8xl font-black">Otso Saarinen</h1>
-                    <h2 className="text-4xl font-bold text-blue-500 italic">
+                    <h1 className="text-honeydew text-8xl font-black">
+                        Otso Saarinen
+                    </h1>
+                    <h2 className="text-naples-yellow text-4xl font-bold italic">
                         Bachelor of Engineering
                     </h2>
                 </div>
-                <div className="max-w-md text-xl md:max-w-2xl">
+                <div className="text-honeydew max-w-md text-xl md:max-w-2xl">
                     <Typewriter
                         text="I'm a recently graduated ICT engineer from Turku UAS,
                     interested about web development & health technology.
@@ -55,7 +57,7 @@ function Title() {
                     />
                 </div>
                 <div className="flex flex-col gap-3">
-                    <div className="text-4xl font-bold text-blue-500">
+                    <div className="text-honeydew text-4xl font-bold">
                         Skills
                     </div>
                     <div className="grid grid-cols-2 gap-5 md:grid-cols-4">
@@ -64,7 +66,7 @@ function Title() {
                             const singleSkill = skill;
                             return (
                                 <div key={index}>
-                                    <div className="text-2xl font-bold text-blue-500">
+                                    <div className="text-naples-yellow text-2xl font-bold">
                                         {categoryName}
                                     </div>
                                     <div className="text-xl">
