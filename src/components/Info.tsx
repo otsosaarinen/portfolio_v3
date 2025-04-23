27e6afe005519be192867de114f17255ffb427c2
Otso Saarinen <saarinenotso@gmail.com>
2025-04-24 00:29:29 +0300

    Create Info.tsx

---
+++ src/components/Info.tsx
@@ -0,0 +1,24 @@
+function Info() {
+    return (
+        <>
+            <div className="flex flex-row justify-between font-medium text-white">
+                <div className="my-5 flex w-200 items-center justify-center bg-indigo-500 p-5 text-xl">
+                    I'm a recently graduated ICT engineer from Turku UAS,
+                    passionate about technology and motivated to start my career
+                    in IT industry. I'm very interested about web development &
+                    health technology.
+                </div>
+                <div>
+                    <div className="my-5 flex w-65 items-center justify-center bg-indigo-500 p-5 text-xl">
+                        Azure AZ-900 Certified
+                    </div>
+                    <div className="my-5 flex w-65 items-center justify-center bg-indigo-500 p-5 text-xl">
+                        Azure AZ-900 Certified
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Info;
