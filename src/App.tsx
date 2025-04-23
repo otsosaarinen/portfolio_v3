2fb3cc221821a6bd0c0ad301a47b9fa8925f5140
Otso Saarinen <saarinenotso@gmail.com>
2025-04-23 16:45:02 +0300

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
 import Content from "./components/Content";
-import Footer from "./components/Footer";
 
 function App() {
     return (
