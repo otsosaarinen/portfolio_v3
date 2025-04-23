c813929f5c9eaa651fb305cf7b3965da7e3e5f05
Otso Saarinen <saarinenotso@gmail.com>
2025-04-23 16:30:52 +0300

    add my name to content file

---
+++ src/components/Content.tsx
@@ -0,0 +1,15 @@
+function Content() {
+    return (
+        <>
+            <div className="text-white">
+                <h1 className="text-8xl font-black">Otso Saarinen</h1>
+                <h2 className="text-3xl font-bold italic">
+                    Bachelor of Engineering, Information and Communications
+                    Technology
+                </h2>
+            </div>
+        </>
+    );
+}
+
+export default Content;
