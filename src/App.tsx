d6a0ee4771ab5e1c63f28132c70a8863b1d0b181
Otso Saarinen <saarinenotso@gmail.com>
2025-04-28 17:42:48 +0300

    remove text color css to main div

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import Info from "./components/Info";
 function App() {
     return (
         <>
-            <div className="font-nunito-sans h-screen w-screen bg-[url('/coding.jpg')] bg-cover bg-no-repeat">
-                <div className="flex h-full w-full flex-col bg-neutral-900/70 text-neutral-800">
+            <div className="font-nunito-sans h-screen w-screen bg-[url('/coding.jpg')] bg-cover bg-no-repeat text-neutral-800">
+                <div className="flex h-full w-full flex-col bg-neutral-900/70">
                     <Header />
                     <Info />
                     <Skills />
