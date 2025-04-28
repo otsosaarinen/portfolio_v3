8556349c11fadb95d367317d5fc05bd1b89fe288
Otso Saarinen <saarinenotso@gmail.com>
2025-04-28 17:39:15 +0300

    move y-margins to main div

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -8,11 +8,11 @@ function Info() {
                     in IT industry. I'm very interested about web development &
                     health technology.
                 </div>
-                <div>
-                    <div className="my-5 flex w-65 items-center justify-center bg-indigo-500 p-5 text-xl">
+                <div className="my-5 flex flex-col gap-5">
+                    <div className="flex w-65 items-center justify-center bg-indigo-500 p-5 text-xl">
                         Azure AZ-900 Certified
                     </div>
-                    <div className="my-5 flex w-65 items-center justify-center bg-indigo-500 p-5 text-xl">
+                    <div className="flex w-65 items-center justify-center bg-indigo-500 p-5 text-xl">
                         Bachelor of Engineering
                     </div>
                 </div>
