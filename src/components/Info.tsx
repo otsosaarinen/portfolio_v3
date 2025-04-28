a296c16dbbaf994d05cec4c0054bbfa50a8be17a
Otso Saarinen <saarinenotso@gmail.com>
2025-04-28 17:46:41 +0300

    move margin to parent div

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,14 +1,14 @@
 function Info() {
     return (
         <>
-            <div className="flex flex-row justify-between font-medium text-white">
-                <div className="my-5 flex w-220 items-center justify-center bg-indigo-500 p-5 text-xl">
+            <div className="my-5 flex flex-row justify-between font-medium text-white">
+                <div className="flex w-220 items-center justify-center bg-indigo-500 p-5 text-xl">
                     I'm a recently graduated ICT engineer from Turku UAS,
                     passionate about technology and motivated to start my career
                     in IT industry. I'm very interested about web development &
                     health technology.
                 </div>
-                <div className="my-5 flex flex-col gap-5">
+                <div className="flex flex-col gap-5">
                     <div className="flex w-65 items-center justify-center bg-indigo-500 p-5 text-xl">
                         Azure AZ-900 Certified
                     </div>
