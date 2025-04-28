6bd66d152625b8a44c82b418f108064553c61d88
Otso Saarinen <saarinenotso@gmail.com>
2025-04-28 17:39:41 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
 import Skills from "./components/Skills";
-import Projects from "./components/Projects";
 import Info from "./components/Info";
 
 function App() {
