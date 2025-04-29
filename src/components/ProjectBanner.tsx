b7aa6451a3909dc1fd89f0adbe029b4a5c533003
Otso Saarinen <saarinenotso@gmail.com>
2025-04-29 20:59:35 +0300

    add links to projects button

--- src/components/ProjectBanner.tsx
+++ src/components/ProjectBanner.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router";
+
 function ProjectBanner() {
     return (
         <>
             <div className="my-5 w-110 bg-white p-5 text-center text-2xl font-bold text-indigo-500 transition duration-300 hover:cursor-pointer hover:bg-indigo-500 hover:text-white">
-                Click here to view my projects
+                <Link to="/projects">Click here to view my projects</Link>
             </div>
         </>
     );
